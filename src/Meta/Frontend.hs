badadcb1fbf1279a905c8604ce29e30b36b55373
Kaptch <kaptch@gmail.com>
2020-06-22 18:15:53 +0300

    missing file fix

--- src/Meta/Frontend.hs
+++ src/Meta/Frontend.hs
@@ -3,13 +3,11 @@ module Meta.Frontend (
   module Meta.Frontend.ParMeta,
   module Meta.Frontend.AbsMeta,
   module Meta.Frontend.PrintMeta,
-  module Meta.Frontend.ErrM,
-  module Meta.Frontend.LayoutMeta
+  module Meta.Frontend.ErrM
   ) where
 
 import           Meta.Frontend.AbsMeta
 import           Meta.Frontend.ErrM
-import           Meta.Frontend.LayoutMeta
 import           Meta.Frontend.LexMeta
 import           Meta.Frontend.ParMeta
 import           Meta.Frontend.PrintMeta
